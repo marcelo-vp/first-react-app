5a6e9e4aacb500a7e52ad9c79f49a18cdd9d7641
marcelo-vp <marcelos.mail@gmail.com>
2018-01-28 21:39:21 -0200

    Added HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';// nosso primeiro componente React!
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		const nomes = ["Lucas", "Tableless", "Leitor", "Maria", "João", "Ana"];
+		return <div>
+			{ nomes.map( ( name ) => <HelloWorld nome={name} /> ) }
+		</div>;
+	}
 }
 
 export default App;
---
+++ src/HelloWorld.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HelloWorld = (props) => <p>Olá {props.nome}!</p>;
+
+export default HelloWorld;
